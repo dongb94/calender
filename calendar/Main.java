24638e2a5fdce78e26a8059497d2e9920cf85efb
dongb94 <dongb94gmail.com>
2017-11-13 02:34:54 +0900

    Main클래스 프레임 및 Today 클래스 구현

--- calendar/Main.java
+++ calendar/Main.java
@@ -1,33 +1,61 @@
 package calendar;
 /* Version 1.0
  * ****************************
- * 최초작성일	2017.11.09 11:30
+ * 최초작성일	2017.11.09 23:30
  * 작성자		변동건
  * ****************************
- * 수정일		2017.11.10 11:30
+ * 수정일		2017.11.10 23:30
  * 수정내용	Calendar 및 Detail 클래스  추가
  * 작성자		변동건
  * ****************************
+ * 수정일		2017.11.12 23:30
+ * 수정내용	Calendar 클래스 변경 -> CalendarPanel
+ * 			해상도에 맞춰서 프레임 사이즈 조절, 프레임 사이즈 변경 불가능하도록 구현
+ * 작성자		변동건
  */
 
 /**메인 프레임*/
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame{
 
+	private double width;
+	private double height;
 	Main(){
-		setSize(1500,800);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		makeGUI();
 		
+		setResizable(false);
 		setVisible(true);
 	}
 	
 	void makeGUI(){
-		Calendar C = new Calendar();
+
+		Dimension res = Toolkit.getDefaultToolkit().getScreenSize();
+		width = res.width*0.8;
+		height = res.height*0.8;
+		setSize((int)(width), (int)(height));
+		setLocation((int)(width*0.1), (int)(height*0.1));
+		setLayout(null);
+		
+		//set calendar
+		CalendarPanel C = new CalendarPanel();
+		C.setSize((int)(width*0.7), (int)(height*0.97));
+		C.setLocation(0, 0);
+		//set today
+		Today T = new Today();
+		T.setSize((int)(width*0.3), (int)(height*0.17));
+		T.setLocation((int)(width*0.7),0);
+		Thread thread = new Thread(T);
+		thread.start();
+		//set detail
 		Detail D = new Detail();
+		D.setSize((int)(width*0.3), (int)(height*0.80));
+		
 		add(C);
+		add(T);
 		add(D);
 	}
 	
--- calendar/Today.java
+++ calendar/Today.java
@@ -4,22 +4,67 @@ package calendar;
  * 최초작성일	2017.11.09 11:30
  * 작성자		변동건
  * ****************************
- * 수정일		2017.11.09 11:30
- * 수정내용	없음
+ * 수정일		2017.11.13 02:31
+ * 수정내용	클래스 함수 및 쓰레드 구현
  * 작성자		변동건
  * ****************************
  */
 
 
+import java.awt.Color;
+import java.awt.Font;
 /**화면 오른쪽 상단 현재시간을 보여주는 클래스*/
 import java.util.Date;
-import javax.swing.JPanel;
 
-public class Today extends JPanel{
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Today extends JPanel implements Runnable{
+	private Date d;
+	private JLabel now;
 	Today(){
-		Date d =new Date();
+		d =new Date();
+		makeGUI();
+		updateText();
 	}
-	void makeGUI(){
+	private void makeGUI(){
+		now = new JLabel();
+		Font f = now.getFont();
+		f = new Font(f.getFamily(), f.getStyle(), 20);
+		now.setFont(f);
+		add(now);
 		
+		setVisible(true);
+	}
+	private void updateText(){
+		now.setSize(getSize());
+		String time = null;
+		if(d.getHours()<10)
+			time = "0"+d.getHours(); 
+		else
+			time = ""+d.getHours();
+
+		if(d.getMinutes()<10)
+			time += ":0"+d.getMinutes();
+		else
+			time += ":"+d.getMinutes();
+		
+		if(d.getSeconds()<10)
+			time += ":0"+d.getSeconds();
+		else
+			time += ":"+d.getSeconds();
+		
+		now.setText((d.getYear()+1900)+"."+(d.getMonth()+1)+"."+d.getDate()+"("+d.toString().substring(0,3)+")"+" "+time);
+	}
+	public void run() {
+		while(true){
+			updateText();
+			repaint();
+			try{
+				Thread.sleep(1000);
+			}catch (InterruptedException e) {
+				System.err.println(e);
+			}
+		}
 	}
 }
