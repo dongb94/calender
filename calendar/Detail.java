378f08f02082e7bbd8d6822343cb62973c5ce82d
ARA <lemonic3520@naver.com>
2017-11-13 07:55:00 +0900

    Revert "Main에서 Detail 패널 위치 조정, Detail, ScheduleList 작성중"
    This reverts commit 89020e8ac3399b435838fcc6ae4e38db60f841ad.

--- calendar/Detail.java
+++ calendar/Detail.java
@@ -1,36 +1,7 @@
 package calendar;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-
 import javax.swing.JPanel;
 
 public class Detail extends JPanel{
-	
-	public ScheduleList scheduleList = null;
-	public Modify modify = null;
-	
-	Detail() {
-		setLayout(new BorderLayout());
-		
-		scheduleList = new ScheduleList(this);
-		
-		add(this.scheduleList, BorderLayout.CENTER);
-		setVisible(true);
-	}
-	
-	public void change(String panelName) {
-		if (panelName.equals("scheduleList")) {
-			removeAll();
-			add(scheduleList);
-			revalidate();
-			repaint();
-		} else {
-			removeAll();
-			add(modify);
-			revalidate();
-			repaint();
-		}
-	}
-	
+
 }
--- calendar/ScheduleList.java
+++ calendar/ScheduleList.java
@@ -1,26 +1,7 @@
 package calendar;
 
-import java.awt.FlowLayout;
-import java.util.ArrayList;
-
-import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ScheduleList extends JPanel{
-	
-	private Detail parent;
-	private JButton add, delete;
-	private ArrayList<Schedule> list;
-	private Schedule schedule;
-	
-	ScheduleList(Detail parent) {
-		this.parent = parent;
-		setLayout(new FlowLayout(FlowLayout.RIGHT));
-	
-		add = new JButton("추가");
-		delete = new JButton("삭제");
-		add(add);
-		add(delete);
-	}
-	
+
 }
