f204442f835e2d4cfba5119d4985cfb1b07523b7
문성철 <msc2624@gmail.com>
2017-11-14 02:15:24 +0900

    3차 수정 DB 그냥 jsp파일에서 get으로 받아서 디비에 저장 디비 불러 올경우는 html안에 json형식으로 저장 시켜 파싱해오는 방식

---
+++ calendar/AsyncDownThread.java
@@ -0,0 +1,67 @@
+﻿package calendar;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+import java.net.URL;
+import java.util.logging.Handler;
+
+
+
+public class AsyncDownThread extends Thread {
+	public static String result;
+	private String addr;
+
+	public AsyncDownThread(String addr) {
+		this.addr = addr;
+
+	}
+	public static String removeTag(String html) throws Exception {
+		return html.replaceAll("<[^>]*>", "").replaceAll(" ", "").replaceAll("\n", "").replaceAll("\t", "");
+
+	}
+
+
+	public void run() {
+		result = downloadHtml(addr);
+		try {
+			result = removeTag(result);
+			System.out.println(result);
+		//********************************스레드 통신 필요한부분 result를 메인스레드로 가져오기만 하면됨
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("url접속완료");	
+		
+
+	}
+
+	private String downloadHtml(String addr) {
+		StringBuilder html = new StringBuilder();
+		try {
+			URL url = new URL(addr);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			if (conn != null) {
+				conn.setConnectTimeout(10000);
+				conn.setUseCaches(false);
+				if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+					BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+					while (true) {
+						String line = br.readLine();
+						if (line == null)
+							break;
+						html.append(line+"\n");
+						
+					}
+					br.close();
+				}
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return html.toString();
+	}
+}
--- calendar/DB.java
+++ calendar/DB.java
@@ -1,54 +1,31 @@
-package calendar;
+﻿package calendar;
 
+import java.io.PipedReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-/*
-create table( day varchar(100), title varchar(100), contents varchar(100));
-테이블 생성하는 sql문
-*/
 public class DB {
 	Schedule sch;
+
 	public DB(Schedule sch) {
 		this.sch = sch;
 	}
-	
-	static Connection conn;
-	
-	public static void DBINFO() throws Exception {
-		String url = "jdbc:oracle:thin:@192.168.35.50:1521:xe";
-		String dbid = "system";
-		String dbpw = "2624";
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		conn = DriverManager.getConnection(url, dbid, dbpw);
-	}
 
-	public static int addDaySchedule(Schedule schedule) throws Exception {
-		DBINFO();
-		PreparedStatement pst = conn.prepareStatement("insert into Calendar values(?,?,?)");
-		pst.setString(1, schedule.getDate());
-		pst.setString(2, schedule.getTitle());
-		pst.setString(3, schedule.getContents());
-		int cnt = pst.executeUpdate();
-		return cnt;// 리턴값 int인 이유는 추가 되었을떄 cnt>0 이상일경우에 디비에 추가가 성공됨을 알 수 있다.
+	public static void ADDWEBDB(Schedule sch) throws Exception {
+		String url = String.format(
+				"http://192.168.35.50:8083/DataBaseProject/WindowPro.jsp?date=%s&title=%s&contents=%s", sch.getDate(),
+				sch.getTitle(), sch.getContents());
+		AsyncDownThread thread = new AsyncDownThread(url);
+		thread.start();
 	}
-
-	public static ArrayList<Schedule> getDaySchedule() throws Exception {
-		DBINFO();
-			
-		ArrayList<Schedule> sch_list = new ArrayList<>();
-		PreparedStatement pst = conn.prepareStatement("select * from Calendar");
-		ResultSet rs = pst.executeQuery();
-		while (rs.next()) {
-			String date = rs.getString(1);
-			String title = rs.getString(2);
-			String contents = rs.getString(3);
-			sch_list.add(new Schedule(date,title,contents));
-		}
-		return sch_list;
+	
+	public static void getData() throws Exception{
+		String url = "http://192.168.35.50:8083/DataBaseProject/WindowPro.jsp?";
+		AsyncDownThread thread2 = new AsyncDownThread(url);
+		thread2.start();
 
 	}
 }
