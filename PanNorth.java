5e6a75bd43d53393c07b83a53320fdf3c7c091dd
gilseong <mrhgs@naver.com>
2017-11-13 21:09:41 +0900

    11/13 오후 9시 수정

--- PanNorth.java
+++ PanNorth.java
@@ -9,70 +9,92 @@ import java.awt.event.ActionListener;
 import java.util.Calendar;
 
 public class PanNorth extends JPanel{
-    private CalendarPanel uP;
-    private Calendar today;
-    private JButton btnBefore, btnAfter;
-    private JTextField textYear, textMonth;
+	private CalendarPanel uP;
+	private Calendar today;
+	private JButton btnToday, btnMonthBefore, btnMonthAfter, btnYearBefore, btnYearAfter;
+	private JTextField textYear, textMonth;
 
-    //Constructor #Entry Point
-    public PanNorth(CalendarPanel upperPanel){
-        this.uP = upperPanel;
-        this.today = uP.getToday();
-        Font font = uP.getDefaultFont();
-
-        btnBefore = new JButton("◀");
-        textYear = new JTextField(today.get(Calendar.YEAR)+"년");
-        textYear.setEnabled(false);
-        textMonth = new JTextField(today.get(Calendar.MONTH)+1+"월", 3);
-        textMonth.setEnabled(false);
-        btnAfter = new JButton("▶");
-
-        textYear.setFont(font);
-        textMonth.setFont(font);
-
-        this.add(btnBefore);
-        this.add(textYear);
-        this.add(textMonth);
-        this.add(btnAfter);
+	//Constructor #Entry Point
+	public PanNorth(CalendarPanel upperPanel){
+		this.uP = upperPanel;
+		this.today = uP.getToday();
+		Font font = uP.getDefaultFont();
 
 
-        btnBefore.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                today.add(Calendar.MONTH, -1);
-                uP.setToday(today.getTime());
-                uP.reload();
-            }
-        });
-        
-        btnAfter.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                today.add(Calendar.MONTH, 1);
-                uP.setToday(today.getTime());
-                uP.reload();
-            }
-        });
+		btnYearBefore = new JButton("◀◀");
+		btnMonthBefore = new JButton("◀");
+		textYear = new JTextField(today.get(Calendar.YEAR)+"년");
+		textYear.setEnabled(false);
+		textMonth = new JTextField(today.get(Calendar.MONTH)+1+"월", 3);
+		textMonth.setEnabled(false);
+		btnMonthAfter = new JButton("▶");
+		btnYearAfter = new JButton("▶▶");
+
+		textYear.setFont(font);
+		textMonth.setFont(font);
+
+		this.add(btnYearBefore);
+		this.add(btnMonthBefore);
+		this.add(textYear);
+		this.add(textMonth);
+		this.add(btnMonthAfter);
+		this.add(btnYearAfter);
 
 
-        textYear.addCaretListener(new CaretListener() {
-            @Override
-            public void caretUpdate(CaretEvent e) {
-                //regex + update
-            }
-        });
-        textMonth.addCaretListener(new CaretListener() {
-            @Override
-            public void caretUpdate(CaretEvent e) {
-                //regex + update
-            }
-        });
-    }
-    public void reload(){
-        this.today = uP.getToday();
-        textYear.setText(today.get(Calendar.YEAR)+"년");
-        textYear.setEnabled(false);
-        textMonth.setText(today.get(Calendar.MONTH)+1+"월");
-        textYear.setEnabled(false);
-    }
+		btnMonthBefore.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				today.add(Calendar.MONTH, -1);
+				uP.setToday(today.getTime());
+				uP.reload();
+			}
+		});
+
+		btnMonthAfter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				today.add(Calendar.MONTH, 1);
+				uP.setToday(today.getTime());
+				uP.reload();
+			}
+		});
+
+
+		btnYearBefore.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				today.add(Calendar.YEAR, -1);
+				uP.setToday(today.getTime());
+				uP.reload();
+			}
+		});
+
+		btnYearAfter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				today.add(Calendar.YEAR, 1);
+				uP.setToday(today.getTime());
+				uP.reload();
+			}
+		});
+
+
+//		textYear.addCaretListener(new CaretListener() {
+//			@Override
+//			public void caretUpdate(CaretEvent e) {
+//				//regex + update
+//			}
+//		});
+//		textMonth.addCaretListener(new CaretListener() {
+//			@Override
+//			public void caretUpdate(CaretEvent e) {
+//				//regex + update
+//			}
+//		});
+	}
+	public void reload(){
+		this.today = uP.getToday();
+		textYear.setText(today.get(Calendar.YEAR)+"년");
+		textMonth.setText(today.get(Calendar.MONTH)+1+"월");
+	}
 }
