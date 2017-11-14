c66f5f82822a3f1728879a630ee95ea19fc3470a
ARA <lemonic3520@naver.com>
2017-11-15 03:22:51 +0900

    ScheduleList 새로고침 구현 중

--- calendar/ScheduleList.java
+++ calendar/ScheduleList.java
@@ -20,12 +20,13 @@ import javax.swing.JScrollPane;
 
 public class ScheduleList extends JPanel{
 	
-	private Detail d;
+	private Detail detail;
+	private ScheduleList sc;
 	private ArrayList<Schedule> list= new ArrayList<>();
 	private ArrayList<Schedule> dayList= new ArrayList<>();
 	private Schedule schedule;
 	private DB db;
-	private String start_time, end_time;
+	private String start_time, end_time, date;
 	private JLabel blank;
 	private JScrollPane scroll;
 	private JPanel btn_panel, list_panel;
@@ -39,11 +40,9 @@ public class ScheduleList extends JPanel{
 	private String date_piece[] = new String[7];
 	
 	private Modify m;
-	
-	ScheduleList(Detail detail) {
+	ScheduleList() {
 		this.setBackground(new Color(0,0,0,150));
-
-		this.d = detail;
+		this.sc = this;
 		db = new DB();
 		btn_panel = new JPanel();
 		list_panel = new JPanel();
@@ -61,12 +60,22 @@ public class ScheduleList extends JPanel{
 		btn_panel.add(add);
 		btn_panel.add(delete);
 		add(btn_panel, BorderLayout.NORTH);
+	}
+	ScheduleList(Detail detail) {
+		this();
+		this.detail = detail;
 		
 		SimpleDateFormat today = new SimpleDateFormat(); 
-	    today.applyPattern("yyyy.MM.dd"); 
+	    today.applyPattern("yyyy.MM.dd");
+	    
 		setList(today.format(new Date()));
 	}
-	
+	ScheduleList(Detail detail, String date) {
+		this();
+		this.detail = detail;
+		
+		setList(date);
+	}
 	public void setList(String date) {
 		try {
 			// 0:년  1:월  2:일  3:시작시간  4:시작분  5:끝난시간  6:끝난분
@@ -74,6 +83,7 @@ public class ScheduleList extends JPanel{
 			StringTokenizer datest;
 			int i, j, k;
 			list = db.getDaySchedule();
+			this.date = date;
 
 			k=0;
 			while (argst.hasMoreElements()) {
@@ -82,7 +92,6 @@ public class ScheduleList extends JPanel{
 			}
 
 			for (i=0; i<list.size(); i++) {
-				System.out.println("test");
 				datest = new StringTokenizer(list.get(i).getDate());
 				k=0;
 				while (datest.hasMoreElements()) {
@@ -97,19 +106,23 @@ public class ScheduleList extends JPanel{
 					}
 				}
 			}
-			System.out.println(dayList.size());
+			
+			if (dayList.size() == 0) {
+				JLabel notice = new JLabel("일정이 없습니다. 추가해주세요.");
+				list_panel.add(notice);
+				add(list_panel);
+				return;
+			}
+			
 			b_list = new JButton[dayList.size()]; //dayList.size()
 			c_list = new JCheckBox[dayList.size()];
 			st_list = new JLabel[dayList.size()];
 			et_list = new JLabel[dayList.size()];
 			t_list = new JPanel[dayList.size()];
 			p_list = new JPanel[dayList.size()];
-			
-			System.out.println("aaaaaaaaa");
+
 			for (j=0; j<dayList.size(); j++) { //dayList.size()
-				System.out.println("dayList");
 				if (dayList.isEmpty() == false) {
-					System.out.println("dayListExist");
 					datest = new StringTokenizer(dayList.get(j).getDate());
 					k=0;
 					while (argst.hasMoreElements()) {
@@ -120,7 +133,6 @@ public class ScheduleList extends JPanel{
 					st_list[j] = new JLabel(date_piece[3] + " : " + date_piece[4]); //date_piece[3] + " : " + date_piece[4]
 					et_list[j] = new JLabel(date_piece[5] + " : " + date_piece[6]); //date_piece[5] + " : " + date_piece[6]
 					b_list[j] = new JButton(dayList.get(j).getTitle()); //dayList.get(j).getTitle()
-					//System.out.println(dayList.get(j).getTitle());
 					c_list[j] = new JCheckBox();
 					t_list[j] = new JPanel();
 					p_list[j] = new JPanel();
@@ -144,14 +156,12 @@ public class ScheduleList extends JPanel{
 					p_list[j].add(b_list[j]);
 					p_list[j].add(c_list[j]);
 
-					list_panel.add(p_list[j]);					
+					list_panel.add(p_list[j]);
 				}
 			}
 			scrollPane = new JScrollPane(list_panel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 			scrollPane.setBorder(null);
 			add(scrollPane);
-			
-			this.updateUI();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -166,15 +176,26 @@ public class ScheduleList extends JPanel{
 			// TODO Auto-generated method stub
 			JButton btn = (JButton) e.getSource();
 			if (btn.getText().equals("추가")) {
-				d.change("modify");
-				d.setCreate();
+				detail.change("modify");
+				detail.setCreate();
 				System.out.println("this called 추가 : "+arg_piece[0]+arg_piece[1]+arg_piece[2]);
-				d.ifCre(arg_piece[0],arg_piece[1],arg_piece[2]);
+				detail.ifCre(arg_piece[0],arg_piece[1],arg_piece[2]);
 				 //년, 월, 일 인자 던져야함
+				sc.removeAll();
+				sc.setList(date);
+				sc.repaint();
+				//d.change("scheduleList");
 			} else if (btn.getText().equals("삭제")) {
 				for (int i=0; i<30; i++) { // list.size()만큼 해야함
 					if (c_list[i].isSelected()) {
-						// db.deleteSchedule(list[i])
+						try {
+							db.deleteSchedule(list.get(i));
+							//setList(date);
+							//d.change("scheduleList");
+						} catch (Exception e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 					}
 				}
 			}
@@ -189,9 +210,9 @@ public class ScheduleList extends JPanel{
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			JButton btn = (JButton) e.getSource();
-			d.change("modify");
-			d.setModify();
-			d.ifMod();
+			detail.change("modify");
+			detail.setModify();
+			detail.ifMod();
 		}
 		
 	}
