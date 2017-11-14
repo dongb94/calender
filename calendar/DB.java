f67caa5af2407000ce7600fd01ee0fc50c001aa9
dongb94 <dongb94gmail.com>
2017-11-14 20:47:46 +0900

    DB수정 oracle ->mysql

--- calendar/DB.java
+++ calendar/DB.java
@@ -9,31 +9,29 @@ import java.util.ArrayList;
 
 public class DB {
 	Schedule sch;
-<<<<<<< HEAD
-
-=======
-	
+	static Connection conn;
 	public DB() {
 		
 	}
-	
->>>>>>> 653a93ea0f68d918ed718bc723135880dac46cdd
+
 	public DB(Schedule sch) {
 		this.sch = sch;
 	}
-
-	public static void ADDWEBDB(Schedule sch) throws Exception {
-		String url = String.format(
-				"http://192.168.35.50:8083/DataBaseProject/WindowPro.jsp?date=%s&title=%s&contents=%s", sch.getDate(),
-				sch.getTitle(), sch.getContents());
-		AsyncDownThread thread = new AsyncDownThread(url);
-		thread.start();
+	public static void getInfo() throws Exception {
+		String url = "jdbc:mysql://168.131.153.176:3306/calendar";
+		String dbid = "superuser";
+		String dbpw = "1234";
+		Class.forName("com.mysql.jdbc.Driver");
+		conn = DriverManager.getConnection(url, dbid, dbpw);
 	}
 	
-	public static void getData() throws Exception{
-		String url = "http://192.168.35.50:8083/DataBaseProject/WindowPro.jsp?";
-		AsyncDownThread thread2 = new AsyncDownThread(url);
-		thread2.start();
-
-	}
+	 public static int addDaySchedule(Schedule sch) throws Exception {
+			getInfo();
+			PreparedStatement pst = conn.prepareStatement("insert into Calendar values(?,?,?)");
+			pst.setString(1, sch.getTitle());
+			pst.setString(2, sch.getDate());
+			pst.setString(3, sch.getContents());
+			int cnt = pst.executeUpdate();
+			return cnt;// 리턴값 int인 이유는 추가 되었을떄 cnt>0 이상일경우에 디비에 추가가 성공됨을 알 수 있다.
+		}
 }
