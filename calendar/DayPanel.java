7ff77a097588aa683e9f3b5a7163f5779737d364
DELL-INSPIRON75\yimsb <beankuu@gmail.com>
2017-11-14 21:41:22 -0700

    request completed, multiple titles in a box

--- calendar/DayPanel.java
+++ calendar/DayPanel.java
@@ -4,60 +4,77 @@ package calendar;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 
 public class DayPanel extends JPanel {
 
 	private JLabel centerLabel = new JLabel();
 	private String date;
-	private JLabel title = new JLabel();
+	private ArrayList<JLabel> titles = new ArrayList<JLabel>();
+	private PanSouth uP;
 
 	Detail D;
 
-
-	public DayPanel(){
+	public DayPanel(PanSouth uP){
+		this.uP = uP;
+		Font font = centerLabel.getFont();
+		font = new Font(font.getFamily(),font.getStyle(),30);
+		centerLabel.setFont(new Font(font.getFamily(),font.getStyle(),30));
 		this.add(centerLabel, BorderLayout.CENTER);
-		this.add(title, BorderLayout.SOUTH);
+		for(JLabel label: titles) {
+			label.setFont(new Font(font.getFamily(), font.getStyle(), 24));
+			this.add(label, BorderLayout.SOUTH);
+		}
+
 		this.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent e){
 				DayPanel DayPanel = (DayPanel)e.getSource();
 				D.setScheduleList(DayPanel.date);
 			}
 		});
-		Font font = centerLabel.getFont();
-		font = new Font(font.getFamily(),font.getStyle(),30);
-		centerLabel.setFont(new Font(font.getFamily(),font.getStyle(),30));
-		title.setFont(new Font(font.getFamily(),font.getStyle(),24));
+
 		//ImageIcon img = new ImageIcon("image/day.png");
 		//Image image = img.getImage(); // transform it 
 		//Image newimg = image.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
 		//img = new ImageIcon(newimg);  // transform it back
 		//centerLabel.setIcon(img);
 	}
-	// String longer than 15 char is goin' to be cut down
-	public void setTitle(String title) {
-	    if(title.length() > 15)
-	        title = title.substring(0,15)+"...";
-		this.title.setText(title);
-	}
-	public DayPanel(String text){
-		this();
+	public DayPanel(PanSouth uP, String text){
+		this(uP);
 		centerLabel.setText(text);
 		this.setForeground(new Color(255,255,255));
 		this.setBackground(new Color(0,0,0,150));
 	}
-	public DayPanel(String text,Color color){
-		this();
+	public DayPanel(PanSouth uP, String text,Color color){
+		this(uP);
 		centerLabel.setText(text);
 		this.setForeground(new Color(255,255,255));
 		this.setBackground(color);
+	}
+	//get Title data from PanSouth
+	public void setTitles(){
+		 Iterator it = uP.getTitlesFromList(date).iterator();
+		 while(it.hasNext())
+			 addText((String)it.next());
+	}
+	public void resetTitle(){
+		for(JLabel labels: titles)
+			this.remove(labels);
 	}
 	public void setText(String text){
 		centerLabel.setText(text);
 		this.setBackground(new Color(0,0,0));
 	}
+	// Allow only 3 texts
 	public void addText(String text){
-		this.add(new JLabel("* "+text), BorderLayout.SOUTH);
+		if(titles.size() < 3) {
+			if (text.length() > 13)
+				text = text.substring(0, 13) + "...";
+			titles.add(new JLabel("* " + text));
+			this.add(titles.get(titles.size()-1), BorderLayout.SOUTH);
+		}
 	}
 	@Override
 	public void setForeground(Color fg) {
--- calendar/PanSouth.java
+++ calendar/PanSouth.java
@@ -10,13 +10,13 @@ public class PanSouth extends JPanel{
     private CalendarPanel uP;
     //Sunday ~ Saturday
     private DayPanel[] dayNames = {
-            new DayPanel("일",new Color(255,0,0,150)),
-            new DayPanel("월"),
-            new DayPanel("화"),
-            new DayPanel("수"),
-            new DayPanel("목"),
-            new DayPanel("금"),
-            new DayPanel("토",new Color(0,0,255,150))
+            new DayPanel(this,"일",new Color(255,0,0,150)),
+            new DayPanel(this,"월"),
+            new DayPanel(this,"화"),
+            new DayPanel(this,"수"),
+            new DayPanel(this,"목"),
+            new DayPanel(this,"금"),
+            new DayPanel(this,"토",new Color(0,0,255,150))
     };
     // day 1 ~ 28/29(Feb) 30/31 + blanks
     private DayPanel[] dayBlock = new DayPanel[42];
@@ -40,7 +40,7 @@ public class PanSouth extends JPanel{
         for(DayPanel day: dayNames)
             this.add(day);
         for(int i=0; i<dayBlock.length; ++i) {
-            dayBlock[i] = new DayPanel();
+            dayBlock[i] = new DayPanel(this);
             this.add(dayBlock[i]);
         }
     }
@@ -48,20 +48,36 @@ public class PanSouth extends JPanel{
     public void dayInit(){
         for(DayPanel day: dayBlock){
             day.setText("");
-
-            day.setTitle("");
-
+            day.resetTitle();
             day.setDetailPane(uP.getDetailPane());
-
         }
         Calendar firstDay = uP.getToday();
         firstDay.set(Calendar.DAY_OF_MONTH, firstDay.getActualMinimum(Calendar.DAY_OF_MONTH));
         int blockCount = firstDay.get(Calendar.DAY_OF_WEEK)-firstDay.getFirstDayOfWeek();
         Date d = new Date();
 
+        // 1. set Titles and Dates for this month
+        for(int i = 0; i< firstDay.getActualMaximum(Calendar.DAY_OF_MONTH); ++i) {
+            dayBlock[i + blockCount].setDate(String.valueOf(firstDay.get(Calendar.YEAR)+"/"+(firstDay.get(Calendar.MONTH)+1)+"/"+firstDay.get(Calendar.DATE)));
+            firstDay.add(Calendar.DATE, 1);
+            //Sunday
+            if((i+blockCount)%7 == 0) dayBlock[i + blockCount].setForeground(new Color(255,0,0));
+                //Saturday
+            else if((i+blockCount)%7 == 6) dayBlock[i + blockCount].setForeground(new Color(0,0,255));
+                //other
+            else dayBlock[i + blockCount].setForeground(new Color(255,255,255));
+            dayBlock[i + blockCount].setText(String.valueOf(i + 1));
+            dayBlock[i + blockCount].setTitles();
+        }
+    }
+    // Callback Function for DayPanel
+    public ArrayList<String> getTitlesFromList(String date){
+        Calendar receivedDay =Calendar.getInstance();
+        try {
+            receivedDay.setTime((new SimpleDateFormat("yyyy/MM/dd")).parse(date));
+        }catch(Exception e){}
 
-        // 1.get Titles only for this month
-        HashMap<String, Schedule> scheduleList = new HashMap<String, Schedule>();
+        ArrayList<String> strList = new ArrayList<String>();
         Iterator it = totalScheduleList.iterator();
         Schedule tempSchedule;
         Calendar calTemp = Calendar.getInstance();
@@ -69,44 +85,12 @@ public class PanSouth extends JPanel{
             try {
                 calTemp.setTime(new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SS")
                         .parse((tempSchedule = (Schedule) it.next()).getDate()));
-                if (calTemp.get(Calendar.YEAR) == firstDay.get(Calendar.YEAR)) {
-                    if (calTemp.get(Calendar.MONTH) == firstDay.get(Calendar.MONTH)) {
-                        scheduleList.put(
-                                String.valueOf(calTemp.get(Calendar.YEAR)+"/"+calTemp.get(Calendar.MONTH)+"/"+calTemp.get(Calendar.DATE)),
-                                        tempSchedule);
-                    } else if (calTemp.get(Calendar.MONTH) >= firstDay.get(Calendar.MONTH))
-                        break;
-                }
+                if (calTemp.get(Calendar.YEAR) == receivedDay.get(Calendar.YEAR) &&
+                        calTemp.get(Calendar.DAY_OF_YEAR) == receivedDay.get(Calendar.DAY_OF_YEAR))
+                            strList.add(tempSchedule.title);
             } catch(Exception e){}
         }
-        // 2. set Titles and Dates for this month
-        calTemp = (Calendar)firstDay.clone();
-        for(int i = 0; i< firstDay.getActualMaximum(Calendar.DAY_OF_MONTH); ++i) {
-            dayBlock[i + blockCount].setDate(String.valueOf(calTemp.get(Calendar.YEAR)+"/"+calTemp.get(Calendar.MONTH)+"/"+calTemp.get(Calendar.DATE)));
-            calTemp.add(Calendar.DATE, 1);
-            //Sunday
-            if((i+blockCount)%7 == 0) dayBlock[i + blockCount].setForeground(new Color(255,0,0));
-                //Saturday
-            else if((i+blockCount)%7 == 6) dayBlock[i + blockCount].setForeground(new Color(0,0,255));
-                //other
-            else dayBlock[i + blockCount].setForeground(new Color(255,255,255));
-            dayBlock[i + blockCount].setText(String.valueOf(i + 1));
-            if(scheduleList.get(dayBlock[i + blockCount].getDate()) != null)
-                dayBlock[i + blockCount].setTitle(scheduleList.get(dayBlock[i + blockCount].getDate()).getTitle());
-        }
-        /*
-        // 1.set Date(1~31) for each panel
-        for(int i = 0; i< firstDay.getActualMaximum(Calendar.DAY_OF_MONTH); ++i) {
-            dayBlock[i + blockCount].setDate(Calendar.YEAR+"."+Calendar.MONTH+"."+(i+1));
-            //Sunday
-            if((i+blockCount)%7 == 0) dayBlock[i + blockCount].setForeground(new Color(255,0,0));
-                //Saturday
-            else if((i+blockCount)%7 == 6) dayBlock[i + blockCount].setForeground(new Color(0,0,255));
-                //other
-            else dayBlock[i + blockCount].setForeground(new Color(255,255,255));
-            dayBlock[i + blockCount].setText(String.valueOf(i + 1));
-        }
-        */
+        return strList;
     }
     // Reloads GUI
     public void reload(){
