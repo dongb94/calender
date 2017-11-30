2278b78eb3cd39e1dc5dc8fc8cfa23a66670e8a1
SungChul Moon <msc2624@gmail.com>
2017-11-30 20:53:21 +0900

    mysql+ MusciPlayer 수정

--- fileSystem/MusicPlayer.java
+++ fileSystem/MusicPlayer.java
@@ -2,8 +2,19 @@ package fileSystem;
 
 public class MusicPlayer {
 	public void playMusicAndVideo(String path) throws Exception {
-		Runtime run = Runtime.getRuntime();
-		String ss= String.format("C:\\Program Files\\Windows Media Player\\wmplayer.exe  %s", path);
-		run.exec(ss);
+			String[] strarr;
+		String newPath = "";
+		strarr = path.split("/");
+		for (int i = 0; i < strarr.length; i++) {
+			newPath += strarr[i] + "\\\\";
+		}
+		newPath=newPath.substring(0, newPath.length()-2);
+		if (newPath == null) {
+			System.out.println("경로명이 명확하지 않습니다(in MusicPlayer");
+		} else {
+			Runtime run = Runtime.getRuntime();
+			String ss = String.format("C:\\Program Files\\Windows Media Player\\wmplayer.exe  %s", newPath);
+			run.exec(ss);
+		}
 	}
 }
