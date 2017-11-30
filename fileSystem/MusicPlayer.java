bc4b67e8344d7d7d475731bb54d3bf5f1f3410a4
SungChul Moon <msc2624@gmail.com>
2017-11-30 20:10:36 +0900

    윈도우 응용프로그램으로 영상과 음악을 실행하는 메소드 추가

--- fileSystem/MusicPlayer.java
+++ fileSystem/MusicPlayer.java
@@ -1,5 +1,9 @@
 package fileSystem;
 
 public class MusicPlayer {
-
+	public void playMusicAndVideo(String path) throws Exception {
+		Runtime run = Runtime.getRuntime();
+		String ss= String.format("C:\\Program Files\\Windows Media Player\\wmplayer.exe  %s", path);
+		run.exec(ss);
+	}
 }
