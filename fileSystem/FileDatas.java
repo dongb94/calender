cb4ac9ea7f81889a78be28efc1fa8c2720130fda
dongb94 <dongb94gmail.com>
2017-12-02 06:09:23 +0900

    FileDatas 기본 생성자 생성, album 이름으로 초기화하는 메소드 생성 즐겨찾기로 초기화하는 메소드 생성

--- fileSystem/FileDatas.java
+++ fileSystem/FileDatas.java
@@ -10,7 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 /**파일 정보를 담고있는 클래스
- * FileDatas(path) 를 통해 초기화
+ * 생성자를 통해 초기화
  * file[]배열을 이용해서 정보 접근*/
 public class FileDatas{
 	
@@ -18,19 +18,23 @@ public class FileDatas{
 	String path;
 	Connection conn = DataBase.conn;
 	
-	FileDatas(String path){
-		this.path = DataBase.Directory_Path_Arrangment(path);
+	/**공백으로 초기화*/
+	FileDatas(){}
+	
+	/**파일 경로로 초기화*/
+	FileDatas(String dir_path){
+		this.path = DataBase.Directory_Path_Arrangment(dir_path);
 		
 		try {
-			PreparedStatement pstc = conn.prepareStatement("select count(*) from file where path='"+path+"'");
-			ResultSet rsc = pstc.executeQuery();
-			if(rsc.next()){
-				int count = rsc.getInt(1);
+			PreparedStatement pst = conn.prepareStatement("select count(*) from file where path='"+path+"'");
+			ResultSet rs = pst.executeQuery();
+			if(rs.next()){
+				int count = rs.getInt(1);
 				file = new FileData[count];
 			}
 			
-			PreparedStatement pst = conn.prepareStatement("select name,favor,type,album,date,thumnail from file where path='"+path+"' ORDER BY date DESC");
-			ResultSet rs = pst.executeQuery();
+			pst = conn.prepareStatement("select name,favor,type,album,date,thumnail from file where path='"+path+"' ORDER BY date DESC");
+			rs = pst.executeQuery();
 			
 			int i = 0;
 			while (rs.next()) {
@@ -48,25 +52,90 @@ public class FileDatas{
 			e.printStackTrace();
 		}
 	}
+	/**엘범명으로 초기화*/
+	void getAlbumFiles(String album){
+		try {
+			PreparedStatement pst = conn.prepareStatement("select sid from album where name='"+album+"'");
+			ResultSet rs = pst.executeQuery();
+			int album_num = 0;
+			if(rs.next()){
+				album_num = rs.getInt(1);
+			}
+			
+			pst = conn.prepareStatement("select count(*) from file where album='"+album_num+"'");
+			rs = pst.executeQuery();
+			if(rs.next()){
+				int count = rs.getInt(1);
+				file = new FileData[count];
+			}
+			
+			pst = conn.prepareStatement("select name,favor,type,path,date,thumnail from file where album='"+album_num+"' ORDER BY date DESC");
+			rs = pst.executeQuery();
+			
+			int i = 0;
+			while (rs.next()) {
+				String name = rs.getString(1);
+				int fv = rs.getInt(2);
+				String type = rs.getString(3);
+				String path = rs.getString(4);
+				long date = rs.getTimestamp(5).getTime();
+				ImageIcon thumnail=null;
+				if(type.equals("img")) thumnail = new ImageIcon(rs.getBytes(6));
+				file[i++]=new FileData(path, name, fv, type, album_num, date, thumnail);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
+	/**즐겨찾기로 초기화*/
+	void getFavoriteFiles(){
+		try {
+			PreparedStatement pst = conn.prepareStatement("select count(*) from file where favor=1");
+			ResultSet rs = pst.executeQuery();
+			if(rs.next()){
+				int count = rs.getInt(1);
+				file = new FileData[count];
+			}
+			
+			pst = conn.prepareStatement("select name,path,type,album,date,thumnail from file where favor=1 ORDER BY date DESC");
+			rs = pst.executeQuery();
+			
+			int i = 0;
+			while (rs.next()) {
+				String name = rs.getString(1);
+				String path = rs.getString(2);
+				String type = rs.getString(3);
+				int img = rs.getInt(4);
+				long date = rs.getTimestamp(5).getTime();
+				ImageIcon thumnail=null;
+				if(type.equals("img")) thumnail = new ImageIcon(rs.getBytes(6));
+				file[i++]=new FileData(path, name, 1, type, img, date, thumnail);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		//단위 테스트용 main클래스
 		new DataBase();
 		
-		FileData fd = new FileData("/test", "/20160722_053051.jpg");
-		System.out.println("date = " + fd.date);
-		System.out.println("favor = " + fd.favor);
-		System.out.println("img = " + fd.img);
-		System.out.println("dir = " + fd.dir);
-		System.out.println("vid = " + fd.vid);
+//		FileData fd = new FileData("/test", "/20160722_053051.jpg");
+//		System.out.println("date = " + fd.date);
+//		System.out.println("favor = " + fd.favor);
+//		System.out.println("img = " + fd.img);
+//		System.out.println("dir = " + fd.dir);
+//		System.out.println("vid = " + fd.vid);
 		
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		frame.setSize(200, 200);
-		JLabel l = new JLabel(fd.thumnail);
-		frame.add(l);
-		frame.setVisible(true);
+//		JFrame frame = new JFrame();
+//		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+//		frame.setSize(200, 200);
+//		JLabel l = new JLabel(fd.thumnail);
+//		frame.add(l);
+//		frame.setVisible(true);
 		
 		System.out.println("------------------------------");
 		FileDatas fds = new FileDatas("");
@@ -78,6 +147,31 @@ public class FileDatas{
 		System.out.println("img = " + fds.file[0].img);
 		System.out.println("dir = " + fds.file[0].dir);
 		System.out.println("vid = " + fds.file[0].vid);
+		System.out.println("------------------------------");
+
+		FileDatas album = new FileDatas();
+		album.getAlbumFiles("dog");
+		
+		System.out.println("name = " + album.file[1].name);
+		System.out.println("path = " + album.file[1].path);
+		System.out.println("date = " + album.file[0].date);
+		System.out.println("favor = " + album.file[0].favor);
+		System.out.println("img = " + album.file[0].img);
+		System.out.println("dir = " + album.file[0].dir);
+		System.out.println("vid = " + album.file[0].vid);
+		System.out.println("------------------------------");
+		
+		FileDatas favor = new FileDatas();
+		favor.getFavoriteFiles();
+		
+		System.out.println("name = " + favor.file[0].name);
+		System.out.println("path = " + favor.file[0].path);
+		System.out.println("date = " + favor.file[0].date);
+		System.out.println("favor = " + favor.file[0].favor);
+		System.out.println("img = " + favor.file[0].img);
+		System.out.println("dir = " + favor.file[0].dir);
+		System.out.println("vid = " + favor.file[0].vid);
+		System.out.println("------------------------------");
 	}
 }
 
