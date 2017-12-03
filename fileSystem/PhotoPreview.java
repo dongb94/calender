96a73bb7a029fda6df1a713b6dd46ceb0f3ad4b8
SungChul Moon <msc2624@gmail.com>
2017-12-03 21:53:22 +0900

    프리뷰 오류 제거

--- fileSystem/PhotoPreview.java
+++ fileSystem/PhotoPreview.java
@@ -1,18 +1,21 @@
 package fileSystem;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class PhotoPreview {
-			ImageIcon originIcon = new ImageIcon(path);
+public class PhotoPreview extends JFrame {
+	public PhotoPreview(String path) {
+		ImageIcon originIcon = new ImageIcon(path);
 		System.out.println(path);
 		Image orign = originIcon.getImage();
-		Image changedImage = orign.getScaledInstance(500,500, Image.SCALE_SMOOTH);
+		Image changedImage = orign.getScaledInstance(500, 500, Image.SCALE_SMOOTH);
 		ImageIcon Icon = new ImageIcon(changedImage);
 		JLabel jl = new JLabel(Icon);
 		add(jl);
-		setSize(500,500);
+		setSize(500, 500);
 		setTitle(path);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		setResizable(true);
+	}
 }
