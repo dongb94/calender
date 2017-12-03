53b022283df689a0006be826d0403541b4a10606
SungChul Moon <msc2624@gmail.com>
2017-12-03 21:50:03 +0900

    프리뷰 제작

--- fileSystem/PhotoPreview.java
+++ fileSystem/PhotoPreview.java
@@ -1,5 +1,18 @@
 package fileSystem;
+import javax.swing.*;
+import java.awt.*;
 
 public class PhotoPreview {
-
+			ImageIcon originIcon = new ImageIcon(path);
+		System.out.println(path);
+		Image orign = originIcon.getImage();
+		Image changedImage = orign.getScaledInstance(500,500, Image.SCALE_SMOOTH);
+		ImageIcon Icon = new ImageIcon(changedImage);
+		JLabel jl = new JLabel(Icon);
+		add(jl);
+		setSize(500,500);
+		setTitle(path);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setResizable(true);
 }
